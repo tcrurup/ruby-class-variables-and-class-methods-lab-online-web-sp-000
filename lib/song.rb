f186f9a34cb13d36976c070788e2b511c2eedbe9
Anthony Rurup <tcrurup@gmail.com>
2019-04-30 05:49:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,12 +3,15 @@ class Song
   attr_accessor :name, :artist, :genre
   
   @@count = 0
+  @@genres = []
   
   def initialize(song_name, artist, genre)
     @name = song_name
     @artist = artist
     @genre = genre
     @@count += 1
+    
+    if @@genres.contain
   end
   
   def self.count
